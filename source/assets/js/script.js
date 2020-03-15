2574d91de70fb90e1892d99d05eb4a0c1937fad5
vetneka <44982805+vetneka@users.noreply.github.com>
2020-03-15 19:02:55 +0400

    feat(header) - add fixed header and moving through anchors

--- source/assets/js/script.js
+++ source/assets/js/script.js
@@ -1,3 +1,57 @@
-'use strict';
+"use strict";
 
-console.log('Hello, script', 'Hi');
+(function() {
+  const mainNav = document.querySelector(".main-nav");
+  const mainNavItems = mainNav.querySelectorAll(".main-nav__item");
+
+  window.addEventListener("load", () => {
+    mainNav.addEventListener("click", mainNavClickHandler);
+  });
+
+  const mainNavClickHandler = evt => {
+    if (evt.target.classList.contains("main-nav__link")) {
+      setInitialStateMainNav();
+      removeAcitveClasses();
+      setActiveClass(evt);
+      scrollToAnchor(evt);
+    }
+  };
+
+  const setInitialStateMainNav = () => {
+    mainNavItems.forEach(item => {
+      item.classList.remove("main-nav__item--active");
+
+      let itemLink = item.querySelector(".main-nav__link");
+      itemLink.setAttribute("href", `#${itemLink.textContent.toLowerCase()}`);
+    });
+  };
+
+  const removeAcitveClasses = () => {
+    mainNavItems.forEach(item => {
+      item.classList.remove("main-nav__item--active");
+    });
+  };
+
+  const setActiveClass = evt => {
+    evt.target
+      .closest(".main-nav__item")
+      .classList.add("main-nav__item--active");
+  };
+
+  const scrollToAnchor = evt => {
+    evt.preventDefault();
+
+    let mainHeader = document.querySelector(".main-header");
+    let mainHeaderHeight = mainHeader.getBoundingClientRect().height;
+
+    let anchor = evt.target.hash;
+    let elementToScroll = document.querySelector(anchor);
+    let elementToScrollPositionY =
+      elementToScroll.getBoundingClientRect().top + window.scrollY;
+
+    location.hash = anchor;
+
+    scrollTo(0, elementToScrollPositionY - mainHeaderHeight);
+  };
+})();
+
